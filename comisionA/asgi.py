39a900661c9dbd0b0b40b42c2b142af23c639c8d
FacuMint <facundolemo@gmail.com>
2026-04-26 02:47:40 -0300

    rename project to comisionA

--- comisionA/asgi.py
+++ comisionA/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for comisionB project.
+ASGI config for comisionA project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comisionB.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comisionA.settings')
 
 application = get_asgi_application()
